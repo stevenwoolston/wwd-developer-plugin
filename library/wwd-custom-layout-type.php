06f5c94a6fd464d984e380bb04b41fe299a39f84
Steven Woolston <design@woolston.com.au>
2019-02-05 20:34:44 +1000

    menu Icon
    allowed custom dashicon

--- library/wwd-custom-layout-type.php
+++ library/wwd-custom-layout-type.php
@@ -7,9 +7,9 @@
 
 if ( ! defined('ABSPATH')) exit;  // if direct access 
 
-function wwd_add_custom_post($name, $label, $pluralisedName) 
+function wwd_add_custom_post($name, $label, $pluralisedName, $icon = 'dashicons-editor-table') 
 {
-    wwd_generic_custom_layout_type($name, $label, $pluralisedName);
+    wwd_generic_custom_layout_type($name, $label, $pluralisedName, $icon);
 }
 
 function wwd_options_custom_post_type() {
@@ -31,8 +31,8 @@ function wwd_options_custom_post_type() {
 }
 add_action( 'init', 'wwd_options_custom_post_type' );
 
-function wwd_generic_custom_layout_type($name, $label, $plural) {
-
+function wwd_generic_custom_layout_type($name, $label, $plural, $icon = 'dashicons-editor-table') 
+{
     $labels = array(
         'name' => _x( $label, $name ),
         'singular_name' => _x( $label, $name ),
@@ -54,7 +54,7 @@ function wwd_generic_custom_layout_type($name, $label, $plural) {
         'public' => true,
         'show_ui' => true,
         'show_in_menu' => true,
-        'menu_icon' => 'dashicons-editor-table',
+        'menu_icon' => $icon,
         'show_in_nav_menus' => true,
         'publicly_queryable' => true,
         'exclude_from_search' => false,
