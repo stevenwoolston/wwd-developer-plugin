57c5f0d7b9da4a3e2ec99b144cf4afd3d7402111
Steven Woolston <design@woolston.com.au>
2019-02-13 21:32:09 +1000

    Fixed register menu code

--- library/wwd-register-theme-support.php
+++ library/wwd-register-theme-support.php
@@ -28,3 +28,8 @@ $custom_background = $options['use_custom_background'];
 if ($custom_background == 1) {
     add_theme_support('custom-background');
 }
+
+function wwd_register_menus() {
+    register_nav_menu('nav-menu', 'Navigation Main Menu');
+}
+add_action( 'after_setup_theme', 'wwd_register_menus' );
