a30a0f5d6a289e6d8b060f513e1e3453d01c220f
Steven Woolston <steven.woolston@gmail.com>
2019-02-17 21:50:07 +1000

    Widget Support
    Added a default sidebar widget

--- library/wwd-register-theme-support.php
+++ library/wwd-register-theme-support.php
@@ -30,6 +30,22 @@ if ($custom_background == 1) {
 }
 
 function wwd_register_menus() {
-    register_nav_menu('nav-menu', 'Navigation Main Menu');
+    register_nav_menu('primary', 'Header Navigation Menu');
 }
 add_action( 'after_setup_theme', 'wwd_register_menus' );
+
+function wwd_widget_setup() {
+    register_sidebar(
+        array(
+            'name' => 'Site Wide Sidebar',
+            'id' => 'global-sidebar',
+            'class' => 'wwd-sidebar',
+            'description' => 'Standard Sidebar',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h1 class="widget-title">',
+            'after_title' => '</h1>'
+        )
+    );
+}
+add_action('widgets_init', 'wwd_widget_setup');
