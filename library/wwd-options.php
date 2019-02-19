53e4d044ebacb228932d899b31eec9917c2637be
Steven Woolston <steven.woolston@gmail.com>
2019-02-19 20:22:46 +1000

    get attachment method

--- library/wwd-options.php
+++ library/wwd-options.php
@@ -19,6 +19,32 @@ function wwd_theme_create_settings_page() {
     require_once WWD_PLUGIN_PATH . "/templates/admin.php";
 }
 
+function wwd_get_attachment($num = 1) {
+	$output = '';
+	if (has_post_thumbnail() && $num == 1):
+		//	get the featured image
+		$output = wp_get_attachment_url( get_post_thumbnail_id(get_the_ID()) );
+	else:
+		//	get the attached images from the post
+		$attachments = get_posts(array(
+			'post_type' => 'attachment',
+			'posts_per_page' => $num,
+			'post_parent' => get_the_ID()
+		));
+
+		if ($attachments && $num == 1):
+			foreach($attachments as $attachment):
+				$output = wp_get_attachment_url( $attachment->ID );
+			endforeach;
+		elseif ($attachments && $num > 1):
+			$output = $attachments;
+		endif;
+
+		wp_reset_postdata();
+	endif;
+
+	return $output;
+}
 /*
 add_action( 'admin_menu', 'wwd_add_options_page' );
 function wwd_add_options_page() {
